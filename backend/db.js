dbf3a6d24ed50184c64ab39c2f81f74e22ee1a24
ZarinaAttaria <146745823+ZarinaAttaria@users.noreply.github.com>
2024-08-19 12:35:35 +0500

    added mongo Url to env

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://Zareesha:zareesha123@cluster0.kkb4bos.mongodb.net/BudgetTracker"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`Connected to MongoDb database ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error in MongoDb ${error}`);
