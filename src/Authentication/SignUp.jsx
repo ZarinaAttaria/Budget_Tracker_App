fc6c273450f54dddd6c0ae5eb9d99d14376bc8d8
ZarinaAttaria <146745823+ZarinaAttaria@users.noreply.github.com>
2024-08-20 11:43:50 +0500

    updated sign up page

--- src/Authentication/SignUp.jsx
+++ src/Authentication/SignUp.jsx
@@ -42,73 +42,75 @@ function SignUp() {
   return (
     <>
       <div className="mainSignUpContainer">
-        <div className="signUpContainer1">
-          <img src="signUpImage.png" className="signUpImage" />
-          <div className="signUpContainer2">
-            <h1>Sign Up</h1>
-            <Link to="/login" className="loginLink">
-              Already have an account?
-            </Link>
+        <div className="SignUpContainer">
+          <div className="signUpContainer1">
+            <img src="signUpImage.png" className="signUpImage" />
+            <div className="signUpContainer2">
+              <h1>Sign Up</h1>
+              <Link to="/login" className="loginLink">
+                Already have an account?
+              </Link>
 
-            <form onSubmit={handleSubmit}>
-              <div className="signUpForm">
-                <label>
-                  <input
-                    className="input"
-                    type="text"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    placeholder="First Name"
-                  />
-                </label>
-                <label>
-                  <input
-                    className="input"
-                    type="text"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    placeholder=" Last Name"
-                  />
-                </label>
-                <label>
-                  <input
-                    className="input"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Email"
-                  />
-                </label>
-                <label>
-                  <input
-                    className="input"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                  />
-                </label>
-                <label>
-                  <input
-                    className="input"
-                    type="password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    placeholder="Confirm Password"
-                  />
-                </label>
-                <label>
-                  <input
-                    className="input"
-                    type="number"
-                    value={budgetLimit}
-                    onChange={(e) => setBudgetLimit(e.target.value)}
-                    placeholder=" Budget Limit"
-                  />
-                </label>
-                <input type="Submit" className="SignUpButton" />
-              </div>
-            </form>
+              <form onSubmit={handleSubmit}>
+                <div className="signUpForm">
+                  <label>
+                    <input
+                      className="input"
+                      type="text"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                      placeholder="First Name"
+                    />
+                  </label>
+                  <label>
+                    <input
+                      className="input"
+                      type="text"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                      placeholder=" Last Name"
+                    />
+                  </label>
+                  <label>
+                    <input
+                      className="input"
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="Email"
+                    />
+                  </label>
+                  <label>
+                    <input
+                      className="input"
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Password"
+                    />
+                  </label>
+                  <label>
+                    <input
+                      className="input"
+                      type="password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      placeholder="Confirm Password"
+                    />
+                  </label>
+                  <label>
+                    <input
+                      className="input"
+                      type="number"
+                      value={budgetLimit}
+                      onChange={(e) => setBudgetLimit(e.target.value)}
+                      placeholder=" Budget Limit"
+                    />
+                  </label>
+                  <input type="Submit" className="SignUpButton" />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
